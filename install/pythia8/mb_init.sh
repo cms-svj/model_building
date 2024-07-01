968d1221a732d4443159f1f815888e425c899038
Jakia Chowdhury <jakiac@cmslpc325.fnal.gov>
2024-07-01 16:58:38 -0500

    Removed pdfs, pkls and source codes

--- install/pythia8/mb_init.sh
+++
@@ -1,4 +0,0 @@
-export PYTHIA8=${MODEL_BUILDING}/install/pythia8
-export PATH=${PYTHIA8}/bin/:${PATH}
-export PYTHIA8DATA=$(pythia8-config --xmldoc)
-export PYTHIA8RUNNER=${PYTHIA8}/examples/main42
--- install/python_packages/mb_init.sh
+++
@@ -1,1 +0,0 @@
-export PYTHONPATH=${MODEL_BUILDING}/install/python_packages/.local/lib/python3.9/site-packages/:${PYTHONPATH}
