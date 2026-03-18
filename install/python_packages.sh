e2824d07a66c33748da754feb5e8fbbdda679cb5
Kevin Pedro <kpedro88@gmail.com>
2026-03-18 17:49:22 -0500

    add fastjet; install new packages after upgrades

--- install/python_packages.sh
+++ install/python_packages.sh
@@ -3,14 +3,6 @@
 mkdir -p python_packages
 cd python_packages
 
-PKGS=(
-magiconfig \
-)
-
-for PKG in ${PKGS[@]}; do
-	HOME=$PWD pip install --user --no-cache-dir $PKG
-done
-
 PKGS_UPGRADE=(
 coffea==2025.12.0 \
 mplhep \
@@ -18,6 +10,15 @@ mplhep \
 
 for PKG in ${PKGS_UPGRADE[@]}; do
 	HOME=$PWD pip install --user --no-cache-dir --upgrade $PKG
+done
+
+PKGS=(
+magiconfig \
+fastjet \
+)
+
+for PKG in ${PKGS[@]}; do
+	HOME=$PWD pip install --user --no-cache-dir $PKG
 done
 
 cat << 'EOF' > mb_init.sh
