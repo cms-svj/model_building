4de27ae9a435e3563bbd777d3e443f9250264a07
Kevin Pedro <kpedro88@gmail.com>
2026-05-03 12:47:49 -0500

    more options for metaplot

--- Metaplot.py
+++ Metaplot.py
@@ -10,10 +10,10 @@ from magiconfig import ArgumentParser, ArgumentDefaultsRawHelpFormatter
 from glob import glob
 
 samples = [
-	{"name": "FCDC", "models": glob("models/fcdc/s-channel_mmed-1000_Nc-*_Nf-*_scale-10_mq-10.119_mpi-6_mrho-25.0998_pvector-0.5_spectrum-fcdc_gq-0.25_gchi-*_Ns-*")},
-	{"name": "simp", "models": glob("models/fcdc/s-channel_mmed-1000_Nc-3_Nf-3_scale-10_mq-10.119_mpi-6_mrho-25.0998_pvector-0.5_spectrum-fcdcSimp_gq-0.25_gchi-0.333333_rinv-*")},
-	{"name": "FCDC (3-body)", "models": glob("models/fcdc/s-channel_mmed-1000_Nc-*_Nf-*_scale-3.52941_mq-3.8666_mpi-6_mrho-11.2139_pvector-0.5_spectrum-fcdc_gq-0.25_gchi-*_Ns-*")},
-	{"name": "simp (3-body)", "models": glob("models/fcdc/s-channel_mmed-1000_Nc-3_Nf-3_scale-3.52941_mq-3.8666_mpi-6_mrho-11.2139_pvector-0.5_spectrum-fcdcSimp_gq-0.25_gchi-0.333333_rinv-*")},
+    {"name": "FCDC", "models": glob("models/fcdc/s-channel_mmed-1000_Nc-*_Nf-*_scale-10_mq-10.119_mpi-6_mrho-25.0998_pvector-0.5_spectrum-fcdc_gq-0.25_gchi-*_Ns-*")},
+    {"name": "simp", "models": glob("models/fcdc/s-channel_mmed-1000_Nc-3_Nf-3_scale-10_mq-10.119_mpi-6_mrho-25.0998_pvector-0.5_spectrum-fcdcSimp_gq-0.25_gchi-0.333333_rinv-*")},
+    {"name": "FCDC (3-body)", "models": glob("models/fcdc/s-channel_mmed-1000_Nc-*_Nf-*_scale-3.52941_mq-3.8666_mpi-6_mrho-11.2139_pvector-0.5_spectrum-fcdc_gq-0.25_gchi-*_Ns-*")},
+    {"name": "simp (3-body)", "models": glob("models/fcdc/s-channel_mmed-1000_Nc-3_Nf-3_scale-3.52941_mq-3.8666_mpi-6_mrho-11.2139_pvector-0.5_spectrum-fcdcSimp_gq-0.25_gchi-0.333333_rinv-*")},
 ]
 
 # stylistic options
@@ -31,30 +31,19 @@ colors = ["#e42536", "#5790fc", "#964a8b", "#f89c20", "#7a21dd", "#9c9ca1"]
 
 # last two are dashdotdot and dashdashdot
 lines = ["solid", "dashed", "dotted", "dashdot", (0, (3, 5, 1, 5, 1, 5)), (0, (3, 5, 3, 5, 1, 5))]
-custom_cycler = mpl.cycler(color=colors) + mpl.cycler(linestyle=lines)
-
-data = {} # hists + metadata for all models
-
-for sample in samples:
-    data[sample["name"]] = []
-    for model in sample['models']:
-        file = f'{model}/Hists.pkl'
-
-        with open(file, "rb") as inp:
-            data_model = pickle.load(inp)
-
-        data[sample["name"]].append(data_model)
+markers = ['o', 's', 'D', 'v', '^', '*']
+custom_cycler = mpl.cycler(color=colors) + mpl.cycler(linestyle=lines) + mpl.cycler(marker=markers)
 
 # helper to make a plot
-def stat_plot(qname, outdir):
+def stat_plot(data, x, qname, outdir):
     fig, ax = plt.subplots(figsize=(8,6))
     # iterator for manual control
     props = iter(custom_cycler)
     for sample, models in data.items():
         style = next(props)
-        xvals = np.array([model['model']['rinv'] for model in models])
+        xvals = np.array([model['meta'][x] for model in models])
         means, stdevs, stderrs = zip(*[
-            (model['analysis'][qname]['mean'], model['analysis'][qname]['stdev'], model['analysis'][qname]['stderr']) for model in models
+            (model['meta'][qname]['mean'], model['meta'][qname]['stdev'], model['meta'][qname]['stderr']) for model in models
         ])
         # order by rinv_pred
         order = np.argsort(xvals)
@@ -63,28 +52,51 @@ def stat_plot(qname, outdir):
         stdevs = np.array(stdevs)[order]
         stderrs = np.array(stderrs)[order]
         # means
-        line, = ax.plot(xvals, means, 'o-', label=sample, **style)
+        line, = ax.plot(xvals, means, label=sample, fillstyle='none', **style)
         color = line.get_color()
         # stderr as errorbar
         ax.errorbar(xvals, means, yerr=stderrs, fmt='none', ecolor=style['color'], capsize=3, **style)
         # stdev as filled
         ax.fill_between(xvals, means-stdevs, means+stdevs, color=style['color'], alpha=0.15)
+    ax.axline((0,0), slope=1, color='black', linestyle=':')
     ax.set_xlabel(r'$r_{\text{inv}}^{\text{pred}}$')
     ax.set_ylabel(qname)
     ax.legend(framealpha=0.5)
     plt.savefig('{}/stat_{}.pdf'.format(outdir,qname),bbox_inches='tight')
     plt.close(fig)
 
-def make_all_plots(outdir):
+def make_all_plots(outdir, sample_list, x, y):
+    data = {} # hists + metadata for all models
+
+    for sample in samples:
+        if sample_list and sample["name"] not in sample_list: continue
+        data[sample["name"]] = []
+        for model in sample['models']:
+            file = f'{model}/Hists.pkl'
+
+            with open(file, "rb") as inp:
+                data_model = pickle.load(inp)
+                # join these for ease of use
+                data_model['meta'] = data_model['model'] | data_model['analysis']
+
+            data[sample["name"]].append(data_model)
+
     os.makedirs(outdir, exist_ok=True)
-    for qname in ['stability','DHJet12_rinv']:
-        stat_plot(qname, outdir)
+    for qname in y:
+        stat_plot(data, x, qname, outdir)
 
 if __name__=="__main__":
     parser = ArgumentParser(
         formatter_class=ArgumentDefaultsRawHelpFormatter
     )
     parser.add_argument("--dir", type=str, default="All_metaplots", help="output directory")
+    parser.add_argument("--samples", type=str, default=[], nargs='*', help="list of samples to plot")
+    parser.add_argument("-x", type=str, default='rinv', help="x variable")
+    parser.add_argument("-y", type=str, default=['stability','DHJet12_rinv'], nargs='*', help="y variable(s)")
     args = parser.parse_args()
 
-    make_all_plots(args.dir)
+    unknown_samples = [s for s in args.samples if not any([s==sm['name'] for sm in samples])]
+    if unknown_samples:
+        raise ValueError("Unknown sample(s) requested:",','.join(unknown_samples))
+
+    make_all_plots(args.dir, args.samples, args.x, args.y)
